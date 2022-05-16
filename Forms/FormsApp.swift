3608e6dc1e569233caecb5af3be354720f1408ab
Vladimir Fibe <motiw@me.com>
2022-05-16 23:45:56 +0600

    Initial Commit

---
+++ Forms/FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormsApp.swift
+//  Forms
+//
+//  Created by MacService on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
